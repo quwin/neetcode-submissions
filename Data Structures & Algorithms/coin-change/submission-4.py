b6e26fe992a6214e0a6d5a8c6a96fc2ca942a50b
Ethan Tran <ethantran@quwin.dev>
2026-07-19 23:25:26 -0700

    Add: coin-change - submission-4

---
+++ Data Structures & Algorithms/coin-change/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for amm in range(1, amount + 1):
+            for coin in coins:
+                if amm - coin >= 0:
+                    dp[amm] = min(dp[amm], 1 + dp[amm - coin])
+        return dp[amount] if dp[amount] != amount + 1 else -1
